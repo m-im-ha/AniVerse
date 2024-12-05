c447892667ce6f85d40af44631884c3d92176c08
imran <sherlockholmesad221@gmail.com>
2024-12-06 00:29:18 +0600

    add Loading.jsx

---
+++ src/ui/Loading.jsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <span className="loading loading-bars loading-lg"></span>
+    </div>
+  );
+}
+
+export default Loading;
